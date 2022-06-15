a21fbf9854be9e9748ba8b04590842898facf071
David Natanael <david.natanael.2001@gmail.com>
2022-06-16 05:56:55 +0800

    Finished refactoring main.py to cnn.py, util.py, mycallbacks.py and config.yaml

--- cnn.py
+++ cnn.py
@@ -1,12 +1,12 @@
 import os
-import time
 
-import pickle
+import json
 import logging
 from ruamel.yaml import YAML
+from ruamel.yaml.compat import StringIO
 
-from typing import Sequence, Union, Optional, Callable, Iterable, Any
-from functools import partial, reduce
+from typing import Sequence, Union, Optional, Callable
+from functools import partial
 
 import numpy as np
 import pandas as pd
@@ -15,14 +15,15 @@ import matplotlib.pyplot as plt
 # tensorflow
 import tensorflow as tf
 from tensorflow import keras
-from keras import Sequential
-from keras.callbacks import Callback, EarlyStopping, History, ReduceLROnPlateau
+from keras import Sequential, Model
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from keras.layers import \
     BatchNormalization, Conv2D, Dense, Dropout, Flatten, MaxPooling2D, \
     RandomFlip, RandomRotation
 from keras.regularizers import l2
 
 # user-defined
+import mycallbacks
 import util
 
 
@@ -30,65 +31,34 @@ class BeetArmywormDataset(util.ImageDataset):
     def __init__(self, directory, **kwargs):
         super().__init__(directory, **kwargs)
 
-        # assume that all class names in the train and test sets are the same
-        self.class_names: list[str] = self.train_set.class_names
-
-        # normalize the data
         normalize: Callable = partial(
             tf.data.Dataset.map,
             map_func=lambda img, label: (tf.cast(img, tf.float32) / 255, label),
             num_parallel_calls=tf.data.AUTOTUNE)
+
+        # normalize the data
         self.train_set = normalize(self.train_set)
         self.valid_set = normalize(self.valid_set)
         self.test_set = normalize(self.test_set)
 
+        self.train_set.class_names = self.train_set._input_dataset.class_names
+        self.valid_set.class_names = self.valid_set._input_dataset.class_names
+        self.test_set.class_names = self.test_set._input_dataset.class_names
 
-class CNN:
-    model: keras.Model
-    train_history: History
-    _callbacks: list[Callback]
-    save_path: str = './model'
+        self.train_set.file_paths = self.train_set._input_dataset.file_paths
+        self.valid_set.file_paths = self.valid_set._input_dataset.class_names
+        self.test_set.file_paths = self.test_set._input_dataset.class_names
 
-    def __init__(
-            self,
-            input_shape: Sequence,
-            num_classes: int,
-            *,
-            loss: Optional[str | keras.losses.Loss] = None,
-            optimizer: Optional[Union[str | keras.optimizers.Optimizer]] = None,
-            save_path: Optional[str] = None,
-            model_name: Optional[str] = None,
-            load_existing: bool = False,
-            **kwargs):
-        # create the model
-        self.define_model(num_classes, name=model_name)
-        self.model.build(input_shape)
-        self.model.compile(optimizer, loss, metrics=['accuracy'])
-        if load_existing:
-            self.load_weights()
-        logger.info(util.get_model_summary(
-            self.model,
-            line_length=kwargs.get('line_length'),
-            expand_nested=True))
-        logger.info(f'{loss=} {optimizer=}')
 
-        self.callbacks = []
-
-        # create model subdirectory to store model checkpoints
-        self.save_path: str = save_path or self.save_path
-        if not os.path.isdir(self.save_path):
-            os.mkdir(self.save_path)
-            logging.info(f'Created directory {self.save_path}.')
-
-    def define_model(self, num_classes: int, *args, name: Optional[str] = None) -> None:
-        """Define the model."""
+class CNN(Sequential):
+    def __init__(self, num_classes: int, name: str = None):
+        # Define the layers
         preprocessor = Sequential(
-            [
-                RandomFlip(),
-                RandomRotation(factor=0.5),
-                # uncommenting this will cause tf to use a while_loop for a lot of functions
-                # RandomContrast(factor=0.02)
-            ],
+            [RandomFlip(),
+             # uncommenting this will cause tf to use a while_loop for a lot of functions
+             # RandomContrast(factor=0.02),
+             RandomRotation(factor=0.5)
+             ],
             name='preprocessor')
         convolution_layer = Sequential(
             [Conv2D(filters=16, kernel_size=(3, 3), activation='relu',
@@ -113,7 +83,8 @@ class CNN:
              MaxPooling2D(pool_size=(2, 2)),
              Conv2D(filters=128, kernel_size=(3, 3), activation='relu',
                     padding='same', use_bias=False),
-             BatchNormalization()],
+             BatchNormalization()
+             ],
             name='convolution_layer')
         classifier = Sequential(
             [Flatten(),
@@ -126,153 +97,123 @@ class CNN:
              Dense(8, activation='elu', kernel_regularizer=l2(1e-3)),
              Dense(8, activation='elu', kernel_regularizer=l2(1e-3)),
              # output layer, use softmax to convert network output to probabilities
-             Dense(num_classes, activation='softmax')],
+             Dense(num_classes, activation='softmax')
+             ],
             name='classifier')
-        self.model = Sequential(
-            [preprocessor, convolution_layer, classifier],
-            name=name or 'model')
-
-    def train(self, dataset: util.ImageDataset, **kwargs) -> History:
-        """Train the model."""
-        self.train_history: History = self.model.fit(
-            dataset.train_set,
-            validation_data=dataset.valid_set,
-            callbacks=self.callbacks,
-            **kwargs)
-        return self.train_history
-
-    @property
-    def callbacks(self) -> list[Callback]:
-        return self._callbacks
-
-    @callbacks.setter
-    def callbacks(self, value: Union[list[Callback] | Callback]) -> None:
-        if isinstance(value, Callback):
-            self._callbacks = [value]
-        elif isinstance(value, Iterable) \
-                and reduce(lambda a, b: a and isinstance(b, Callback), value, True):
-            self._callbacks = value
-        else:
-            raise ValueError(
-                'Value must be of type `keras.callbacks.Callback` or '
-                '`Iterable[keras.callbacks.Callback]`.')
-
-    @callbacks.deleter
-    def callbacks(self) -> None:
-        self._callbacks = []
-
-    def predict(self, x: Any):
-        return self.model.predict(x)
-
-    def load_weights(self, *, weights_path: Optional[str] = None) -> None:
-        if weights_path is None:
-            weights_path = os.path.join(self.save_path, f'{self.model.name}.h5')
-        try:
-            self.model.load_weights(weights_path)
-        except FileNotFoundError:
-            logger.warning('Model not found. Training a new model.')
-        else:
-            logger.info(f'Loaded weights from {weights_path}.')
-
-    # todo modify
-    def plot_train_history(self) -> plt.Figure:
-        """Plot the model training hisotry."""
-        history = self.train_history.history
-
-        def plotter(ax: plt.Axes, train: str, val: str):
-            ax.plot(history[train], label=f'train')
-            ax.plot(history[val], label=f'validation')
-            ax.set_title('Train History')
-            ax.set_ylabel(train.title())
-            ax.set_xlabel('Epoch')
-            ax.legend(loc='upper left')
-
-        fig: plt.Figure = plt.figure(figsize=(12, 8))
-        gs: plt.GridSpec = fig.add_gridspec(1, 2)
-        ax: plt.Axes = fig.add_subplot(gs[0, 0])
-        plotter(ax, 'accuracy', 'val_accuracy')  # plot accuracy
-        ax: plt.Axes = fig.add_subplot(gs[0, 1])
-        plotter(ax, 'loss', 'val_loss')  # plot loss
-
-        fig.tight_layout()
-        return fig
-
-    def save(self, *, plot_train_history: bool = False) -> None:
-        # check whether the model has been trained
-        if 'train_history' not in self.__dir__():
-            msg = 'This model has not been trained. Train the model first by calling `train()`.'
-            logger.critical(msg)
-            raise RuntimeError(msg)
-
-        filename = os.path.join(self.save_path, f'{self.model.name}')
-
-        # save model architecture as json
-        with open(f'{filename}.json', 'w') as f:
-            f.write(self.model.to_json())
-        logger.info('Model architecture saved successfully.')
-
-        # save model weights as h5
-        self.model.save_weights(f'{filename}.h5')
-        logger.info('Model weights saved successfully.')
-
-        # todo allow the user to choose to save the training history as pickle or as yaml
-        # save model train history as pkl
-        with open(f'{filename}_train_history.pkl', 'wb') as f:
-            pickle.dump(self.train_history.history, f)
-        # save model train history as yaml
-        with open(f'{filename}_train_history.yaml', 'a') as f:
-            now = f'{time.strftime("%Y%m%d %H.%M.%S")}.{(time.time_ns() // 1000) % 1000}'
-            doc = {'timestamp': now, 'train history': self.train_history.history}
-            yaml.explicit_start = True
-            yaml.dump(doc, f)
-            yaml.explicit_start = False
-        logger.info('Train history saved successfully.')
-
-        # save train history as png
-        if plot_train_history:
-            figure: plt.Figure = self.plot_train_history()
-            figure.savefig(f'{filename}_train_history.png', bbox_inches='tight', transparent=True)
+        layers = [preprocessor, convolution_layer, classifier]
+        super().__init__(layers, name)
 
 
-def main(config: dict):
+def create_model(
+        input_shape: Sequence,
+        num_classes: int,
+        *,
+        name: Optional[str] = None,
+        optimizer: Optional[Union[str | keras.optimizers.Optimizer]] = None,
+        loss: Optional[str | keras.losses.Loss] = None) -> CNN:
+    """Create the model."""
+    model = CNN(num_classes, name)
+    model.build(input_shape)
+    model.compile(optimizer, loss, metrics=['accuracy', 'Precision', 'Recall'])
+    logger.info(util.get_model_summary(
+        model,
+        line_length=config.get('logger', {}).get('line_length'),
+        expand_nested=True))
+    logger.info(f'{loss=} {optimizer=}')
+
+    return model
+
+
+def train(
+        model: keras.Model,
+        data: util.ImageDataset,
+        callbacks: dict[str, str],
+        *,
+        save_path: str = None,
+        load_existing: bool = False,
+        **kwargs) -> keras.callbacks.History:
+    save_path = save_path or './model'
+    # create model subdirectory to store model checkpoints
+    if not os.path.isdir(save_path):
+        os.mkdir(save_path)
+        logging.info(f'Created directory {save_path}.')
+    if load_existing:
+        model.load_weights(os.path.join(save_path, f'{model.name}.h5'))
+
+    # set up the callbacks
+    callbacks = [
+        mycallbacks.ReduceLROnPlateau(**callbacks.get('reduce lr on plateau', {})),
+        mycallbacks.EarlyStopping(**callbacks.get('early stop', {})),
+        mycallbacks.RecordWriter()
+    ]
+
+    return model.fit(
+        data.train_set,
+        validation_data=data.valid_set,
+        callbacks=callbacks,
+        **kwargs)
+
+
+def main(configs: list):
+    loader_config, model_config, optimizers = configs
+
     # load the data
-    data = BeetArmywormDataset('./data/beet_armyworm', **config['imageloader'])
+    data = BeetArmywormDataset('./data/beet_armyworm', **loader_config)
 
     # update the configuration
     in_shape, out_shape = map(lambda x: x.shape, data.train_set.element_spec)
-    __ = config.setdefault('model', {}).update({
+    __ = model_config.setdefault('model', {}).update({
         'input_shape': in_shape,
         'num_classes': out_shape[-1]})
 
-    # create the model
-    model = CNN(**config['model'], line_length=config.get('logger', {}).get('line_length'))
-    # todo setup the callbacks
-    # my_callback: "CustomCallback" = ...
-    # reduce_lr_on_plateau: ReduceLROnPlateau = ...
-    # record_writer: "RecordWriter" = ...
-    # early_stop: EarlyStopping = ...
-    # model.callbacks = [my_callback, reduce_lr_on_plateau, record_writer, early_stop]
-    # train the model
-    model.train(data, **config['model']['fit'])
-    model.save(plot_train_history=True)
+    # set up the optimizer
+    optimizer_name: str = model_config['model'].get('optimizer', 'RMSprop')
+    optimizer: keras.optimizers.Optimizer = getattr(keras.optimizers, optimizer_name)
+    model_config['model']['optimizer'] = optimizer(**optimizers.get(optimizer_name, {}))
 
-    # use the models for prediction
-    predictions = pd.Series(np.argmax(model.predict(data), axis=-1))
-    with pd.option_context('display.max_rows', 10):
-        print(f'predictions:\n{predictions}', )
+    # create the model
+    model = create_model(**model_config['model'])
+
+    # train the model
+    history = train(
+        model,
+        data,
+        model_config.get('callbacks'),
+        **model_config['fit'])
+    util.save_model(model, './model')
+    util.save_history(history, './model', model_name=model.name)
+
+    # evaluate the model accuracy
+    scores = model.evaluate(data.test_set, verbose=1, return_dict=True)
+    s_yaml = util.YamlString(typ='safe')
+    s_yaml.default_flow_style = False
+    logger.info(s_yaml.dump({'scores': scores}))
+
+    label_dict = dict(enumerate(data.class_names))
+    logger.info(s_yaml.dump({'labels dictionary': label_dict}))
+    # confusion matrix
+    confusion_matrix = util.confusion_matrix(model, data.test_set)
+    logger.info(f'confusion_matrix:\n{confusion_matrix}')
+
+    # model predictions
+    # todo plot prediction probabilities and their corresponding images
+    s_yaml.default_flow_style = True
+    pred_proba = model.predict(data.test_set)
+    predictions = pd.Series(np.argmax(pred_proba, axis=-1))
+    print(s_yaml.dump({
+        'prediction probabilities': pred_proba.tolist(),
+        'predictions': predictions.tolist()}))
 
 
 logger = util.setup_logging(__name__, add_stream_handler=True)
 if __name__ == '__main__':
-    __, filename = os.path.split(__file__)
-    logger.info(f'Running {filename}')
+    logger.info(f'Running {os.path.split(__file__)[-1]}')
+    logger.info(f'Using GPU {gpu}' if (gpu := tf.test.gpu_device_name()) else f'Using CPU')
 
     yaml = YAML(typ='safe')
     yaml.default_flow_style = False
-    with open('config.yaml', 'r') as f:
-        config, optimizers = yaml.load_all(f)
-    optimizer_name = config['model'].get('optimizer', 'RMSprop')
-    config['model']['optimizer']: keras.optimizers.Optimizer \
-        = getattr(keras.optimizers, optimizer_name)(**optimizers[optimizer_name])
+    with open('config.yaml', 'r') as config_file:
+        config, *model_configs = list(yaml.load_all(config_file))
 
-    main(config)
+    main(model_configs)
+    logger.info(f'Finished running {os.path.split(__file__)[-1]}')
--- util.py
+++ util.py
@@ -1,18 +1,30 @@
+import os
 import sys
+import time
+
+import json
 import logging
-from functools import partial
+from ruamel.yaml import YAML
+from ruamel.yaml.compat import StringIO
+
 from typing import Optional, Callable
+from functools import partial
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
 
 
 # todo allow users to pass custom formatters and handlers
 def setup_logging(
-        logger_name: Optional[str] = None,
-        debug_level: Optional = logging.DEBUG,
+        logger_name: str,
+        log_level: Optional = logging.DEBUG,
         *,
         add_stream_handler=False) -> logging.Logger:
     """Set up the logger."""
     logger = logging.getLogger(logger_name or __name__)
-    logger.setLevel(debug_level)
+    logger.setLevel(log_level)
 
     formatter = logging.Formatter(
         '{asctime}|[{levelname:^8s}]|{name}|{message}', style='{')
@@ -38,8 +50,37 @@ def setup_logging(
     return logger
 
 
+class ImageDataset:
+    def __init__(self, directory, **kwargs):
+        from keras.utils import image_dataset_from_directory
+        _ = kwargs.setdefault('validation_split', 0.2)
+        load: Callable = partial(image_dataset_from_directory, **kwargs)
+
+        dataset = tf.data.Dataset
+        self.train_set: dataset = load(f'{directory}/train', subset='training')
+        self.valid_set: dataset = load(f'{directory}/train', subset='validation')
+        self.test_set: dataset = load(f'{directory}/test', subset=None, validation_split=None)
+        self.class_names = self.train_set.class_names
+
+        num_train_data = len(self.train_set.file_paths)
+        num_valid_data = len(self.valid_set.file_paths)
+        num_test_data = len(self.test_set.file_paths)
+        logger.info(f'Found {num_train_data + num_valid_data + num_test_data} '
+                    f'files belonging to {len(self.train_set.class_names)} classes.')
+        logger.info(f'Using {num_train_data} files for training.')
+        logger.info(f'Using {num_valid_data} files for validation.')
+        logger.info(f'Using {num_test_data} files for testing.')
+
+
+class YamlString(YAML):
+    def dump(self, data, **kwargs) -> str:
+        stream = StringIO()
+        super().dump(data, stream, **kwargs)
+        return stream.getvalue()
+
+
 def get_model_summary(
-        *models: "keras.Model",
+        *models: tf.keras.Model,
         line_length: Optional[int] = 79,
         expand_nested: bool = False) -> str:
     if line_length < 1:
@@ -57,26 +98,78 @@ def get_model_summary(
     return '\n'.join(summary)
 
 
-class ImageDataset:
-    def __init__(self, directory, **kwargs):
-        import tensorflow as tf
-        from keras.utils import image_dataset_from_directory
-        _ = kwargs.setdefault('validation_split', 0.2)
-        load: Callable = partial(image_dataset_from_directory, **kwargs)
+def save_model(model: tf.keras.Model, save_path: str = None) -> None:
+    filename = os.path.join(save_path or './model', f'{model.name}')
 
-        Dataset = tf.data.Dataset
-        self.train_set: Dataset = load(f'{directory}/train', subset='training')
-        self.valid_set: Dataset = load(f'{directory}/train', subset='validation')
-        self.test_set: Dataset = load(f'{directory}/test', subset=None, validation_split=None)
+    # save model architecture as json
+    with open(f'{filename}.json', 'w') as f:
+        f.write(json.dumps(model.to_json(), indent=4))
+        logger.info('Model architecture saved successfully.')
 
-        num_train_data = len(self.train_set.file_paths)
-        num_valid_data = len(self.valid_set.file_paths)
-        num_test_data = len(self.test_set.file_paths)
-        logger.info(f'Found {num_train_data + num_valid_data + num_test_data} '
-                    f'files belonging to {len(self.train_set.class_names)} classes.')
-        logger.info(f'Using {num_train_data} files for training.')
-        logger.info(f'Using {num_valid_data} files for validation.')
-        logger.info(f'Using {num_test_data} files for testing.')
+    # save model weights as h5
+    model.save_weights(f'{filename}.h5')
+    logger.info('Model weights saved successfully.')
 
 
-logger = setup_logging()
+def save_history(
+        history: tf.keras.callbacks.Callback,
+        save_path: str = None,
+        *,
+        model_name: str = None) -> None:
+    filename = os.path.join(save_path or './model', model_name or 'model')
+
+    yaml = YAML(typ='safe')
+    yaml.default_flow_style = False
+    yaml.explicit_start = True
+    # save model train history as yaml
+    with open(f'{filename}_train_history.yaml', 'a') as f:
+        now = f'{time.strftime("%Y%m%d %H.%M.%S")}.{(time.time_ns() // 1000) % 1000}'
+        train_history = {k: list(map(float, v)) for k, v in history.history.items()}
+        doc = {'timestamp': now, 'train history': train_history}
+        yaml.dump(doc, f)
+
+    # save train history as png
+    figure: plt.Figure = plot_train_history(history)
+    figure.savefig(f'{filename}_train_history.png', bbox_inches='tight', transparent=True)
+
+
+def plot_train_history(history: tf.keras.callbacks.History) -> plt.Figure:
+    """Plot the model training hisotry."""
+
+    def plotter(metric: str):
+        ax.plot(history.history[metric], label=f'train')
+        try:
+            ax.plot(history.history[f'val_{metric}'], label=f'validation')
+        except KeyError:
+            pass
+        ax.set_ylabel(metric.title())
+        ax.set_xlabel('Epoch')
+        ax.legend(loc='upper left')
+
+    rows = cols = int(np.ceil(len(history.history)))
+    fig: plt.Figure = plt.figure(figsize=(12, 8))
+    gs: plt.GridSpec = fig.add_gridspec(rows, cols)
+    for i, key in enumerate([k for k in history.history if 'val' not in k]):
+        ax: plt.Axes = fig.add_subplot(gs[divmod(i, cols)])
+        plotter(key)
+
+    fig.suptitle('Train History')
+    fig.tight_layout()
+    return fig
+
+
+def confusion_matrix(model: tf.keras.Model, data: tf.data.Dataset) -> np.ndarray:
+    confusion: tf.Tensor = tf.math.confusion_matrix([], [], len(data.class_names))
+    for images, labels in data:
+        confusion += tf.math.confusion_matrix(
+            labels=np.argmax(labels, axis=-1),
+            predictions=np.argmax(model.predict(images), axis=-1))
+
+    # convert `confusion` into a `pd.DataFrame`
+    confusion: pd.DataFrame = pd.DataFrame(confusion)
+    confusion.index.name = 'label'
+    confusion.columns.name = 'predict'
+    return confusion
+
+
+logger = setup_logging(__name__)
